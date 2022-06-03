b2b04e3c6b2d0d3e1836912988e5ecf18f7c31d6
Sid Shingaki <sidalaytv@gmail.com>
2022-06-02 20:53:55 -0700

    refactored: changed proptextures -> gametextures
    - added lifebar texture2d to gametextures.hpp

--- headers/game.hpp
+++ headers/game.hpp
@@ -11,6 +11,7 @@
 #include "headers/enemy.hpp"
 #include "headers/character.hpp"
 #include "headers/background.hpp"
+#include "headers/gametextures.hpp"
 
 enum class GameState 
 {
---
+++ headers/gametextures.hpp
@@ -0,0 +1,119 @@
+#ifndef GAMETEXTURES_HPP
+#define GAMETEXTURES_HPP
+
+#include <raylib.h>
+
+namespace GameTexture {
+
+    const Texture2D AltarBot{LoadTexture("sprites/props/AltarBot.png")};
+    const Texture2D AltarBotAnimated{LoadTexture("sprites/props/AltarBotAnimated.png")};
+    const Texture2D AltarBotLeft{LoadTexture("sprites/props/AltarLeft.png")};
+    const Texture2D AltarBotLeftAnimated{LoadTexture("sprites/props/AltarLeftAnimated.png")};
+    const Texture2D AltarBotRight{LoadTexture("sprites/props/AltarRight.png")};
+    const Texture2D AltarBotRightAnimated{LoadTexture("sprites/props/AltarRightAnimated.png")};
+    const Texture2D AltarDormant{LoadTexture("sprites/props/AltarDormant.png")};
+    const Texture2D AltarDormantRust{LoadTexture("sprites/props/AltarDormantRust.png")};
+    const Texture2D AltarTop{LoadTexture("sprites/props/AltarTop.png")};
+    const Texture2D AltarTopAnimated{LoadTexture("sprites/props/AltarTopAnimated.png")};
+    const Texture2D AltarTopLeft{LoadTexture("sprites/props/AltarTopLeft.png")};
+    const Texture2D AltarTopLeftAnimated{LoadTexture("sprites/props/AltarTopLeftAnimated.png")};
+    const Texture2D AltarTopRight{LoadTexture("sprites/props/AltarTopRight.png")};
+    const Texture2D AltarTopRightAnimated{LoadTexture("sprites/props/AltarTopRightAnimated.png")};
+
+    const Texture2D AxeStump{LoadTexture("sprites/props/AxeStump.png")};
+    const Texture2D BigGrass{LoadTexture("sprites/props/BigGrass.png")};
+    const Texture2D Boulder{LoadTexture("sprites/props/Boulder.png")};
+
+    const Texture2D BridgeHorizontal{LoadTexture("sprites/props/BridgeHorizontal.png")};
+    const Texture2D BridgeRopeBottom{LoadTexture("sprites/props/BridgeRopeBottom.png")};
+    const Texture2D BridgeRopeLeft{LoadTexture("sprites/props/BridgeRopeLeft.png")};
+    const Texture2D BridgeRopeRight{LoadTexture("sprites/props/BridgeRopeRight.png")};
+    const Texture2D BridgeRopeTop{LoadTexture("sprites/props/BridgeRopeTop.png")};
+    const Texture2D BridgeVertical{LoadTexture("sprites/props/BridgeVertical.png")};
+
+    const Texture2D Bush{LoadTexture("sprites/props/Bush.png")};
+    const Texture2D ClayPot{LoadTexture("sprites/props/ClayPot.png")};
+
+    const Texture2D Door{LoadTexture("sprites/props/Door.png")};
+    const Texture2D DoorBlue{LoadTexture("sprites/props/DoorBlue.png")};
+    const Texture2D DoorRed{LoadTexture("sprites/props/DoorRed.png")};
+
+    const Texture2D DungeonEntrance{LoadTexture("sprites/props/DungeonEntrance.png")};
+
+    const Texture2D FenceDown{LoadTexture("sprites/props/FenceDown.png")};
+    const Texture2D FenceLeft{LoadTexture("sprites/props/FenceLeft.png")};
+    const Texture2D FenceRight{LoadTexture("sprites/props/FenceRight.png")};
+    const Texture2D FenceUp{LoadTexture("sprites/props/FenceUp.png")};
+
+    const Texture2D FlowerBush{LoadTexture("sprites/props/FlowerBush.png")};
+    const Texture2D Flowers{LoadTexture("sprites/props/Flowers.png")};
+
+    const Texture2D GrassAnimation{LoadTexture("sprites/props/GrassAnimation.png")};
+
+    const Texture2D GrassWallBotLeft{LoadTexture("sprites/props/GrassWallBotLeft.png")};
+    const Texture2D GrassWallBotRight{LoadTexture("sprites/props/GrassWallBotRight.png")};
+    const Texture2D GrassBottom{LoadTexture("sprites/props/GrassBottom.png")};
+    const Texture2D GrassWallInnerLeft{LoadTexture("sprites/props/GrassWallInnerLeft.png")};
+    const Texture2D GrassWallInnerRight{LoadTexture("sprites/props/GrassWallInnerRight.png")};
+    const Texture2D GrassWallLeft{LoadTexture("sprites/props/GrassWallLeft.png")};
+    const Texture2D GrassWallRight{LoadTexture("sprites/props/GrassWallRight.png")};
+    const Texture2D GrassWallTop{LoadTexture("sprites/props/GrassWallTop.png")};
+    const Texture2D GrassWallTopLeft{LoadTexture("sprites/props/GrassWallTopLeft.png")};
+    const Texture2D GrassWallTopRight{LoadTexture("sprites/props/GrassWallTopRight.png")};
+
+    const Texture2D Hole{LoadTexture("sprites/props/Hole.png")};
+
+    const Texture2D HouseBlue{LoadTexture("sprites/props/HouseBlue.png")};
+    const Texture2D HouseRed{LoadTexture("sprites/props/HouseRed.png")};
+
+    const Texture2D Interact{LoadTexture("sprites/props/Interact.png")};
+
+    const Texture2D LifebarLeftEmpty{LoadTexture("sprites/enemies/lifebar/round_left_empty.png")};
+    const Texture2D LifebarLeftFilled{LoadTexture("sprites/enemies/lifebar/round_left_filled.png")};
+    const Texture2D LifebarMiddleEmpty{LoadTexture("sprites/enemies/lifebar/round_middle_empty.png")};
+    const Texture2D LifebarMiddleFilled{LoadTexture("sprites/enemies/lifebar/round_middle_empty.png")};
+    const Texture2D LifebarRightEmpty{LoadTexture("sprites/enemies/lifebar/round_right_empty.png")};
+    const Texture2D LifebarRightFilled{LoadTexture("sprites/enemies/lifebar/round_right_empty.png")};
+
+    const Texture2D LargeRocks{LoadTexture("sprites/props/LargeRocks.png")};
+    const Texture2D LittleGrass{LoadTexture("sprites/props/LittleGrass.png")};
+    const Texture2D LittleRocks{LoadTexture("sprites/props/LittleRocks.png")};
+    const Texture2D MediumRocks{LoadTexture("sprites/props/MediumRocks.png")};
+    const Texture2D RockStump{LoadTexture("sprites/props/RockStump.png")};
+
+    const Texture2D Sapling{LoadTexture("sprites/props/Sapling.png")};
+    const Texture2D Sign{LoadTexture("sprites/props/Sign.png")};
+    const Texture2D SpeechBox{LoadTexture("sprites/npc/SpeechBox.png")};
+    const Texture2D SpeechName{LoadTexture("sprites/npc/SpeechName.png")};
+    const Texture2D SquareContainer{LoadTexture("sprites/props/SquareContainer.png")};
+    const Texture2D Stones{LoadTexture("sprites/props/Stones.png")};
+
+    const Texture2D TransparentContainer{LoadTexture("sprites/props/TransparentContainer.png")};
+    const Texture2D TransparentSquare{LoadTexture("sprites/props/TransparentSquare.png")};
+
+    const Texture2D TreasureChest{LoadTexture("sprites/props/TreasureChest.png")};
+    const Texture2D TreasureChestBig{LoadTexture("sprites/props/TreasureChestBig.png")};
+    const Texture2D TreasureHeart{LoadTexture("sprites/props/TreasureHeart.png")};
+
+    const Texture2D TreeBlue{LoadTexture("sprites/props/TreeBlue.png")};
+    const Texture2D TreeClear{LoadTexture("sprites/props/TreeClear.png")};
+    const Texture2D TreeFall{LoadTexture("sprites/props/TreeFall.png")};
+    const Texture2D TreeGreen{LoadTexture("sprites/props/TreeGreen.png")};
+    const Texture2D TreePink{LoadTexture("sprites/props/TreePink.png")};
+    const Texture2D TreeStump{LoadTexture("sprites/props/TreeStump.png")};
+
+    const Texture2D UnderFlowersOne{LoadTexture("sprites/props/UnderFlowersOne.png")};
+    const Texture2D UnderFlowersTwo{LoadTexture("sprites/props/UnderFlowersTwo.png")};
+    const Texture2D UnderFlowersThree{LoadTexture("sprites/props/UnderFlowersThree.png")};
+
+    const Texture2D WallBotLeft{LoadTexture("sprites/props/WallBotLeft.png")};
+    const Texture2D WallBotRight{LoadTexture("sprites/props/WallBotRight.png")};
+    const Texture2D WallBottom{LoadTexture("sprites/props/WallBottom.png")};
+    const Texture2D WallLeft{LoadTexture("sprites/props/WallLeft.png")};
+    const Texture2D WallRight{LoadTexture("sprites/props/WallRight.png")};
+    const Texture2D WallTopLeft{LoadTexture("sprites/props/WallTopLeft.png")};
+    const Texture2D WallTopRight{LoadTexture("sprites/props/WallTopRight.png")};
+    
+}
+
+#endif // gametextures_hpp
