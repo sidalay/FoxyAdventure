9e4e0df52930757848fbc8694593fd30fca61749
Sid Shingaki <sidalaytv@gmail.com>
2022-06-29 19:53:31 -0700

    chore: chest now spawns when defeating finalboss

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -831,7 +831,7 @@ void Enemy::CheckSpawnChest(std::vector<std::vector<Prop>>& Props)
 {
     for (auto& OuterProp:Props) {
         for (auto& Prop:OuterProp) {
-            if (Type == EnemyType::BOSS && Prop.GetType() == PropType::TREASURE) {
+            if ((Type == EnemyType::BOSS || Type == EnemyType::FINALBOSS) && Prop.GetType() == PropType::TREASURE) {
                 if ((Race == EnemyType::CREATURE && !Alive && Prop.GetItemName() == "Top Right Altar Piece") || 
                     (Race == EnemyType::IMP && !Alive && Prop.GetItemName() == "Top Altar Piece") || 
                     (Race == EnemyType::BEHOLDER && !Alive && Prop.GetItemName() == "Bottom Altar Piece") ||
