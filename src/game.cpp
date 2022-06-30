c4b15d5fd06580bac8bcd537642937efcced2ad7
Sid Shingaki <sidalaytv@gmail.com>
2022-06-29 19:42:16 -0700

    fix: treasure spawn
    - fixed treasure worldpos mistake

--- src/game.cpp
+++ src/game.cpp
@@ -2432,8 +2432,8 @@ namespace Game
             Prop{Sprite{Textures.TreasureChest, 4, 1, 1.f/4.f}, Vector2{130.f,210.f}, PropType::TREASURE, Textures, Textures.AltarTopLeft, 4.f, false, true, Progress::ACT_O, PropType::NPC_O, true, "Top Left Altar Piece"},
             Prop{Sprite{Textures.TreasureChest, 4, 1, 1.f/4.f}, Vector2{1025.f,2765.f}, PropType::TREASURE, Textures, Textures.AltarBotLeft, 4.f, false, true, Progress::ACT_O, PropType::NPC_O, true, "Bottom Left Altar Piece"},
             Prop{Sprite{Textures.TreasureChest, 4, 1, 1.f/4.f}, Vector2{2268.f,1797.f}, PropType::TREASURE, Textures, Textures.AltarBot, 4.f, false, true, Progress::ACT_O, PropType::NPC_O, false, "Bottom Altar Piece"},
-            Prop{Sprite{Textures.TreasureChest, 4, 1, 1.f/4.f}, Vector2{2250.f,2550.f}, PropType::TREASURE, Textures, Textures.AltarTopRight, 4.f, false, true, Progress::ACT_O, PropType::NPC_O, true, "Bottom Right Altar Piece"},
-            Prop{Sprite{Textures.TreasureChest, 4, 1, 1.f/4.f}, Vector2{2137.f,1917.f}, PropType::TREASURE, Textures, Textures.Cryptex, 4.f, false, true, Progress::ACT_V, PropType::NPC_DIANA, false, "Cryptex"},
+            Prop{Sprite{Textures.TreasureChest, 4, 1, 1.f/4.f}, Vector2{2137.f,1917.f}, PropType::TREASURE, Textures, Textures.AltarTopRight, 4.f, false, true, Progress::ACT_O, PropType::NPC_O, false, "Bottom Right Altar Piece"},
+            Prop{Sprite{Textures.TreasureChest, 4, 1, 1.f/4.f}, Vector2{3462.f,2760.f}, PropType::TREASURE, Textures, Textures.Cryptex, 4.f, false, true, Progress::ACT_V, PropType::NPC_DIANA, true, "Cryptex"},
 };
         Props.emplace_back(Treasure);
 
